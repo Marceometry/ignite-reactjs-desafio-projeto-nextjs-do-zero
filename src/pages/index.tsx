3c9f9f678e48e05a1a9e9f81044b2ef48e7a933a
Marcelino Teixeira <marcelino.teixeira.dev@gmail.com>
2023-02-10 18:37:47 -0300

    finish styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
+import { format } from 'date-fns';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +27,62 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+const posts: Post[] = [
+  {
+    uid: 'como-utilizar-hooks',
+    first_publication_date: '2021-03-15T19:25:28+0000',
+    data: {
+      title: 'Como utilizar Hooks',
+      subtitle: 'Pensando em sincronização em vez de ciclos de vida',
+      author: 'Joseph Oliveira',
+    },
+  },
+  {
+    uid: 'criando-um-app-cra-do-zero',
+    first_publication_date: '2021-03-25T19:27:35+0000',
+    data: {
+      title: 'Criando um app CRA do zero',
+      subtitle:
+        'Tudo sobre como criar a sua primeira aplicação utilizando Create React App',
+      author: 'Danilo Vieira',
+    },
+  },
+];
+
+export default function Home(): JSX.Element {
+  return (
+    <div className={`${commonStyles.contentContainer} ${styles.homeContainer}`}>
+      <header>
+        <img src="/logo.svg" alt="logo" />
+      </header>
+
+      <main>
+        {posts.map(post => (
+          <Link href={`/post/${post.uid}`}>
+            <a className={styles.post} key={post.uid}>
+              <h1>{post.data.title}</h1>
+              <p>{post.data.subtitle}</p>
+              <div>
+                <span>
+                  <FiCalendar size={18} />
+                  {format(new Date(post.first_publication_date), 'dd MMM yyyy')}
+                </span>
+                <span>
+                  <FiUser size={18} />
+                  {post.data.author}
+                </span>
+              </div>
+            </a>
+          </Link>
+        ))}
+
+        <button className={styles.loadMoreButton} type="button">
+          Carregar mais posts
+        </button>
+      </main>
+    </div>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient({});
